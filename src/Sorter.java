c43013105325c0daf3616e7883e2dce7ed8eee9d
Abdulrahman Bunaiyan <s202064620@kfupm.edu.sa>
2023-02-13 20:03:21 +0300

    Selection method

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,7 +1,32 @@
+import java.util.Arrays;
 public class Sorter {
-    public static void main(String[] args) {
-        
+        public static void selectionSort (int[] list){
+            for (int i = 0; i < list.length; i++){
+                int currentMin = list[i];
+                int currentMinIndex = i; 
+    
+                for (int j = i + 1; j < list.length; j++){
+                    if(currentMin > list[j]){
+                        currentMin = list[j];
+                        currentMinIndex = j;
+                    }
+                }
+    
+                if (currentMinIndex != i){
+                    list[currentMinIndex] = list[i];
+                    list[i] = currentMin;
+                }
+            }
+        }   
+
+
+        public static void main(String[] args) {
+            int[] myArray = {4, 2 ,1 ,6 ,8};
+            selectionSort(myArray);
+            System.out.print(Arrays.toString(myArray)); 
+
+        }
     }
 
     
-}
+
